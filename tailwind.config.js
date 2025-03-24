5e1d99e9f5715c9bfe8bf5b4a4b68a7d2b438a0a
sixtinhoff <franciscomsixto@gmail.com>
2025-03-24 01:11:39 -0300

    upload componet theme select

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    darkMode: 'class', // Esto asegura que el tema oscuro funcione con la clase 'dark'
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}", // Asegúrate de incluir todos los archivos relevantes
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
