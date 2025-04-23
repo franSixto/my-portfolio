ad4dcc5e754a0dde08befb621626aa0441a9bf2d
sixtinhoff <franciscomsixto@gmail.com>
2025-04-23 19:31:34 -0300

    update blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BlogPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-950">
+
+            <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-400">The blog is coming soon</h1>
+            <p className="mt-4 text-lg text-gray-600 dark:text-gray-400">
+                Stay tuned for updates!
+            </p>
+        </div>
+    );
+};
+
+export default BlogPage;
