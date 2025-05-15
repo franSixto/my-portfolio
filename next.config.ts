20218ab0ad028e9b4d6c7ec2b5a1170fabc0424a
sixtinhoff <franciscomsixto@gmail.com>
2025-05-15 15:31:41 -0300

    upload new config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    
     remotePatterns: [
       {
         hostname: "localhost",
@@ -10,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "my-portfolio-cms-production.up.railway.app",
+        hostname: "res.cloudinary.com",
         pathname: "/uploads/**",
       },
     ],
