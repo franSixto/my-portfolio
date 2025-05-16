08f3e5ac600582db38bd97ae6505cb7d9e00f359
sixtinhoff <franciscomsixto@gmail.com>
2025-05-16 13:22:33 -0300

    push projects

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -27,10 +27,10 @@ export default function ProjectCard({
 
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 0.95 }}
-      animate={{ opacity: 1, scale: 1 }}
-      whileInView={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.95 }}
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      whileInView={{ opacity: 1}}
+      exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
       whileHover={{ scale: 1.02 }}
       whileTap={{ scale: 0.98 }}
