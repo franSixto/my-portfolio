1c45a289b363dabdf07f5e1cbf2a0cb8fc8f29e1
sixtinhoff <franciscomsixto@gmail.com>
2025-06-05 11:34:51 -0300

    fix meta image

--- src/app/page.metadata.ts
+++ src/app/page.metadata.ts
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/meta-image.jpg",
+        url: "https://www.fransixto.com.ar/meta-image.jpg",
         width: 1200,
         height: 630,
         alt: "Francisco Sixto Portfolio"
