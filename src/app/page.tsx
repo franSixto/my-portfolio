99fb134056825f5904587ef2ded6afb593f6b139
sixtinhoff <franciscomsixto@gmail.com>
2025-06-05 14:50:07 -0300

    metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ import ContactCTA from "@/components/contact/ContactCTA";
 import Highlights from "@/components/home/Highlights";
 import AboutMeTeaser from "@/components/home/AboutMeTeaser";
 // import ClientsSlider from "@/components/ClientSlider";
+import { metadata } from './page.metadata';
 
+export { metadata };
 
 const Home = () => {
   return (
